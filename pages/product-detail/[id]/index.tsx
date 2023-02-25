36f45c73f7a501fd14c289e393bbb755c68fc475
wahib-sirka <wahib@sirka.io>
2023-02-25 12:41:38 +0700

    chore(product detail): added meta data on product detail page

--- pages/product-detail/[id]/index.tsx
+++ pages/product-detail/[id]/index.tsx
@@ -1,10 +1,21 @@
-import React from 'react';
+import React, { useMemo } from 'react';
 import { NextPage } from 'next';
+import Head from 'next/head';
 import 'twin.macro';
 import ProductDetail from 'components/Container/ProductDetail';
+import { useGetProducts } from 'hooks/products/productsHooks';
+import { ProductDetailDataType } from 'types/productDetailTypes';
 
 const ProductDetailPage: NextPage = () => {
-  return <ProductDetail />;
+  return (
+    <>
+      <Head>
+        <title>MIFX Product Detail</title>
+        <meta name="description" content={''} />
+      </Head>
+      <ProductDetail />
+    </>
+  );
 };
 
 export default ProductDetailPage;
