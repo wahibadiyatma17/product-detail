922d8dd382ba61955acd3faa33ac50729ebc3847
wahib-sirka <wahib@sirka.io>
2023-02-25 08:52:56 +0700

    feat(product-detail): added UI

--- components/Container/ProductDetail/index.tsx
+++ components/Container/ProductDetail/index.tsx
@@ -1,15 +1,70 @@
 import React, { FC, useMemo } from 'react';
+import { MdAddShoppingCart } from 'react-icons/md';
+import Image from 'next/image';
 import 'twin.macro';
-import { useGetProducts, useGetCategory } from 'hooks/products/productsHooks';
+
+import DefaultButton from 'components/Buttons/DefaultButton';
+import { useGetProducts } from 'hooks/products/productsHooks';
 
 const ProductDetail: FC = () => {
-  const { data: productsAPIres } = useGetProducts();
-  const productsData = useMemo(() => productsAPIres?.data ?? {}, []);
+  const { data: productsAPIres, isLoading: isProductsAPILoading } = useGetProducts();
+  const productsData = useMemo(() => productsAPIres?.data ?? [], [productsAPIres]);
+
+  console.log('wahib productsData', productsData);
 
   return (
     <div tw="flex w-full min-h-screen max-w-[1366px] mx-auto">
-      <div tw="flex flex-row h-full w-full min-h-[800px] justify-center items-center bg-red-300">
-        <p>ce</p>
+      <div tw="flex flex-row h-screen w-full justify-center items-center gap-8">
+        {!isProductsAPILoading && (
+          <>
+            <div tw="flex flex-col items-end justify-center gap-8 w-5/12">
+              <div tw="h-full border-[1px] border-solid shadow-md p-8">
+                <div tw="relative w-[25rem] h-[25rem] ">
+                  <Image
+                    src={productsData?.[0]?.image}
+                    alt="product image"
+                    fill
+                    style={{ objectFit: 'cover' }}
+                  />
+                </div>
+              </div>
+              <div>
+                <div tw="flex flex-row gap-6 w-full">
+                  {productsData?.slice(0, 5)?.map((data: any, idx: number) => (
+                    <div tw="h-full border-[1px] border-solid shadow-md p-1" key={idx}>
+                      <div tw="w-[4rem] h-[4rem] relative">
+                        <Image
+                          src={data?.image}
+                          alt={`product ${data?.name}`}
+                          fill
+                          style={{ objectFit: 'cover' }}
+                        />
+                      </div>
+                    </div>
+                  ))}
+                </div>
+              </div>
+            </div>
+
+            <div tw="flex flex-col gap-2 w-7/12 h-[572px] py-12 px-4">
+              <h3 tw="font-semibold text-red-400 text-sm">SALE</h3>
+              <h1 tw="font-bold text-black text-xl">{productsData[0]?.name}</h1>
+              <div tw="flex flex-row gap-2 my-2 w-[200px] h-[20px] bg-red-500"></div>
+              <h3 tw="font-bold text-black text-2xl">{productsData[0]?.price}</h3>
+
+              <div tw="w-full h-[1px] bg-gray-200 my-6 shadow-sm"></div>
+
+              <div tw="flex flex-row gap-4">
+                <DefaultButton
+                  theme="yellow"
+                  text="Add To Cart"
+                  suffix={<MdAddShoppingCart size={24} />}
+                />
+                <DefaultButton theme="green" text="Buy Now" />
+              </div>
+            </div>
+          </>
+        )}
       </div>
     </div>
   );
--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  images: {
+    domains: ["fe.dev.dxtr.asia"],
+  },
 };
 
 module.exports = nextConfig;
